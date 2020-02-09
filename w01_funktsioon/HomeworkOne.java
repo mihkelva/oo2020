dddafcd482d9c6167891a9c4bc61dcd0506ace4f
Mihkel Vaher <mihkelvaher@hotmail.com>
2020-02-09 20:23:44 +0200

    Kodutöö 1

---
+++ w01_funktsioon/HomeworkOne.java
@@ -0,0 +1,19 @@
+public class HomeworkOne{
+
+    public static void main(String[] args){
+
+        String numberOne = "4";
+        String numberTwo = "5";
+
+       String answer = converter(numberOne, numberTwo);
+       System.out.println(answer);
+    }
+
+    static String converter(String numberOne, String numberTwo){
+        int numberOneInt = Integer.parseInt(numberOne);
+        int numberTwoInt = Integer.parseInt(numberTwo);
+        int sum = numberOneInt * numberTwoInt;
+        String stringSum = String.valueOf(sum);
+        return stringSum;
+    };
+}
